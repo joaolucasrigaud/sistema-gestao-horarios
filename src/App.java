ba694433b497ce4ae98a9a6c162a4cbccb770193
Joao Lucas Rigaud <joao.rigaud@hospitalar.local>
2026-05-05 14:13:44 -0300

    build: adiciona pom.xml e configura inicializacao do Spring Boot

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"controllers", "repositories"})
 public class App {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
